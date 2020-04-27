68bcbb0874d9bd92d7b240f11b0b7d4443959f2a
witower <25256638+witower@users.noreply.github.com>
2020-04-27 17:56:19 +0200

    Navigation and main views

---
+++ app/src/main/java/com/pamo/iparish/CustomOfferingFragment.java
@@ -0,0 +1,28 @@
+package com.pamo.iparish;
+
+import android.os.Bundle;
+
+import androidx.fragment.app.Fragment;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+
+/**
+ * A simple {@link Fragment} subclass.
+ */
+public class CustomOfferingFragment extends Fragment {
+
+    public CustomOfferingFragment() {
+        // Required empty public constructor
+    }
+
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_custom_offering, container, false);
+    }
+}
