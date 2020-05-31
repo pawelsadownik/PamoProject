a2f9a8b863dbf35ac18cc2949ba8d74764be85c3
psadownik <p.sadownik@npc.pl>
2020-06-01 01:16:08 +0200

    register refactor

--- app/src/main/java/com/pamo/iparish/register/LoginActivity.java
+++ app/src/main/java/com/pamo/iparish/register/LoginActivity.java
@@ -2,7 +2,6 @@ package com.pamo.iparish.register;
 
 import android.content.Intent;
 
-import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
@@ -12,18 +11,14 @@ import android.widget.EditText;
 import android.widget.TextView;
 import android.widget.Toast;
 
-import com.google.android.gms.tasks.OnCompleteListener;
-import com.google.android.gms.tasks.Task;
-import com.google.firebase.auth.AuthResult;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
 import com.pamo.iparish.HomeActivity;
 import com.pamo.iparish.R;
+import com.pamo.iparish.services.UserService;
 
 public class LoginActivity extends AppCompatActivity {
-  EditText emailId, password;
-  Button btnSignIn;
-  TextView tvSignUp;
+
   FirebaseAuth mFirebaseAuth;
   private FirebaseAuth.AuthStateListener mAuthStateListener;
 
@@ -32,16 +27,20 @@ public class LoginActivity extends AppCompatActivity {
     super.onCreate(savedInstanceState);
     setContentView(R.layout.activity_login);
 
+    UserService userService = new UserService();
+    View view = findViewById(android.R.id.content).getRootView();
+
     mFirebaseAuth = FirebaseAuth.getInstance();
-    emailId = findViewById(R.id.editText);
-    password = findViewById(R.id.editText2);
-    btnSignIn = findViewById(R.id.button2);
-    tvSignUp = findViewById(R.id.textView);
+    EditText emailId = findViewById(R.id.editText);
+    EditText password = findViewById(R.id.editText2);
+    Button btnSignIn = findViewById(R.id.button2);
+    TextView tvSignUp = findViewById(R.id.textView);
 
     mAuthStateListener = firebaseAuth -> {
       FirebaseUser mFirebaseUser = mFirebaseAuth.getCurrentUser();
       if (mFirebaseUser != null) {
         Toast.makeText(LoginActivity.this, getString(R.string.toast_Logged), Toast.LENGTH_SHORT).show();
+
         Intent i = new Intent(LoginActivity.this, HomeActivity.class);
         startActivity(i);
       } else {
@@ -52,29 +51,7 @@ public class LoginActivity extends AppCompatActivity {
     btnSignIn.setOnClickListener(v -> {
       String email = emailId.getText().toString();
       String pwd = password.getText().toString();
-      if (email.isEmpty()) {
-        emailId.setError(getString(R.string.enter_email));
-        emailId.requestFocus();
-      } else if (pwd.isEmpty()) {
-        password.setError(getString(R.string.enter_password));
-        password.requestFocus();
-      } else if (email.isEmpty() && pwd.isEmpty()) {
-        Toast.makeText(LoginActivity.this, getString(R.string.error_empty), Toast.LENGTH_SHORT).show();
-      } else if (!(email.isEmpty() && pwd.isEmpty())) {
-        mFirebaseAuth.signInWithEmailAndPassword(email, pwd).addOnCompleteListener(LoginActivity.this, new OnCompleteListener<AuthResult>() {
-          @Override
-          public void onComplete(@NonNull Task<AuthResult> task) {
-            if (!task.isSuccessful()) {
-              Toast.makeText(LoginActivity.this, getString(R.string.error_again), Toast.LENGTH_SHORT).show();
-            } else {
-              Intent intToHome = new Intent(LoginActivity.this, HomeActivity.class);
-              startActivity(intToHome);
-            }
-          }
-        });
-      } else {
-        Toast.makeText(LoginActivity.this, getString(R.string.error), Toast.LENGTH_SHORT).show();
-      }
+      userService.validateForm(email, pwd, view, LoginActivity.this);
     });
 
     tvSignUp.setOnClickListener(v -> {
---
+++ app/src/main/java/com/pamo/iparish/services/UserService.java
@@ -0,0 +1,101 @@
+package com.pamo.iparish.services;
+
+import android.app.Activity;
+import android.app.Service;
+import android.content.Intent;
+import android.content.res.Resources;
+import android.os.IBinder;
+import android.util.Log;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.Toast;
+
+import androidx.annotation.NonNull;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.firestore.DocumentReference;
+import com.google.firebase.firestore.FirebaseFirestore;
+import com.pamo.iparish.HomeActivity;
+import com.pamo.iparish.R;
+import com.pamo.iparish.register.LoginActivity;
+import com.pamo.iparish.register.MainActivity;
+
+import java.util.HashMap;
+import java.util.Map;
+
+import static androidx.constraintlayout.widget.Constraints.TAG;
+
+public class UserService extends Service {
+
+  FirebaseAuth mFirebaseAuth;
+  FirebaseFirestore fStore;
+  EditText emailId, password;
+  String userID;
+
+  @Override
+  public IBinder onBind(Intent intent) {
+    return null;
+  }
+
+  public void validateForm(String email, String pwd, View view, Activity activity) {
+
+    emailId = view.findViewById(R.id.editText);
+    fStore = FirebaseFirestore.getInstance();
+
+    if (email.isEmpty()) {
+      emailId.setError(Resources.getSystem().getString(R.string.enter_email));
+      emailId.requestFocus();
+    } else if (pwd.isEmpty()) {
+      password.setError(Resources.getSystem().getString(R.string.enter_password));
+      password.requestFocus();
+    } else if (email.isEmpty() && pwd.isEmpty()) {
+      Toast.makeText(activity, activity.getString(R.string.error_empty), Toast.LENGTH_SHORT).show();
+
+    } else if (!(email.isEmpty() && pwd.isEmpty())) {
+      if (activity.getLocalClassName().equals("register.MainActivity")) {
+        createUser(email, pwd, activity);
+      } else {
+        signInUser(email, pwd, activity);
+      }
+
+    } else {
+      Toast.makeText(activity, activity.getString(R.string.error), Toast.LENGTH_SHORT).show();
+    }
+  }
+
+  public void createUser(String email, String pwd, Activity activity) {
+
+    mFirebaseAuth = FirebaseAuth.getInstance();
+    mFirebaseAuth.createUserWithEmailAndPassword(email, pwd).addOnCompleteListener(activity, task -> {
+      if (!task.isSuccessful()) {
+        Toast.makeText(activity, activity.getString(R.string.error_again), Toast.LENGTH_SHORT).show();
+      } else {
+        userID = mFirebaseAuth.getCurrentUser().getUid();
+        DocumentReference documentReference = fStore.collection("users").document(userID);
+        Map<String, Object> user = new HashMap<>();
+        user.put("email", email);
+        documentReference.set(user).addOnSuccessListener((OnSuccessListener) (aVoid) -> {
+          Log.d(TAG, "onSuccess: " + userID);
+          activity.startActivity(new Intent(activity, HomeActivity.class));
+        }).addOnFailureListener(e -> Log.d(TAG, "onFailure: " + e.toString()));
+      }
+    });
+  }
+
+  public void signInUser(String email, String pwd, Activity activity) {
+
+    mFirebaseAuth = FirebaseAuth.getInstance();
+    mFirebaseAuth.signInWithEmailAndPassword(email, pwd).addOnCompleteListener(activity, task -> {
+      if (!task.isSuccessful()) {
+        Toast.makeText(activity, activity.getString(R.string.error_again), Toast.LENGTH_SHORT).show();
+      } else {
+        Intent intToHome = new Intent(activity, HomeActivity.class);
+        activity.startActivity(intToHome);
+      }
+    });
+  }
+}
