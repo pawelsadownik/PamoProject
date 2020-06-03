b1de1d31406ff14de4e3360fffbc59ee92d3a5cf
witower <25256638+witower@users.noreply.github.com>
2020-06-03 14:26:36 +0200

    Proper parish data persistence and loading

--- app/src/main/java/com/pamo/iparish/settings/UserFormFragment.java
+++ app/src/main/java/com/pamo/iparish/settings/UserFormFragment.java
@@ -1,11 +1,6 @@
 package com.pamo.iparish.settings;
 
 import android.os.Bundle;
-
-import androidx.annotation.NonNull;
-import androidx.fragment.app.Fragment;
-import androidx.navigation.fragment.NavHostFragment;
-
 import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
@@ -16,15 +11,15 @@ import android.widget.Button;
 import android.widget.EditText;
 import android.widget.Spinner;
 
-import com.google.android.gms.tasks.OnCompleteListener;
-import com.google.android.gms.tasks.OnSuccessListener;
-import com.google.android.gms.tasks.Task;
+import androidx.fragment.app.Fragment;
+import androidx.navigation.fragment.NavHostFragment;
+
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.firestore.DocumentReference;
 import com.google.firebase.firestore.FirebaseFirestore;
 import com.google.firebase.firestore.QueryDocumentSnapshot;
-import com.google.firebase.firestore.QuerySnapshot;
 import com.pamo.iparish.R;
+import com.pamo.iparish.model.Parish;
 
 import java.util.ArrayList;
 import java.util.HashMap;
@@ -43,9 +38,17 @@ public class UserFormFragment extends Fragment {
     private FirebaseFirestore fStore;
     private FirebaseAuth fAuth;
     private String userID;
+    private DocumentReference usersDocument;
 
     private List<String> churches = new ArrayList<>();
+    private List<Parish> parishes = new ArrayList<>();
     private String church;
+    private Parish parish;
+    private EditText phoneNumber;
+    private EditText name;
+    private EditText surname;
+    private Button saveButton;
+    private Spinner spinner;
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
@@ -60,49 +63,26 @@ public class UserFormFragment extends Fragment {
                              Bundle savedInstanceState) {
         View view = inflater.inflate(R.layout.fragment_user_form, container, false);
 
-        EditText phoneNumber = view.findViewById(R.id.edittext_phone);
-        EditText name = view.findViewById(R.id.edittext_name);
-        EditText surname = view.findViewById(R.id.edittext_surname);
-        Button saveButton = view.findViewById(R.id.button_save);
-        Spinner spinner = view.findViewById(R.id.spinner_parish);
-        DocumentReference usersDocument = fStore.collection("users").document(userID);
+        phoneNumber = view.findViewById(R.id.edittext_phone);
+        name = view.findViewById(R.id.edittext_name);
+        surname = view.findViewById(R.id.edittext_surname);
+        saveButton = view.findViewById(R.id.button_save);
+        spinner = view.findViewById(R.id.spinner_parish);
+        usersDocument = fStore.collection("users").document(userID);
 
-        fStore.collection("churches")
-                .get()
-                .addOnCompleteListener(new OnCompleteListener<QuerySnapshot>() {
-                    @Override
-                    public void onComplete(@NonNull Task<QuerySnapshot> task) {
-                        if (task.isSuccessful()) {
-                            for (QueryDocumentSnapshot document : task.getResult()) {
-                                churches.add(document.getString("name"));
-                            }
-                            if (getActivity() != null) {
-                                usersDocument.addSnapshotListener(getActivity(), (documentSnapshot, e) -> {
-                                    phoneNumber.setText(documentSnapshot.getString("phoneNumber"));
-                                    name.setText(documentSnapshot.getString("name"));
-                                    surname.setText(documentSnapshot.getString("surname"));
-                                    spinner.setSelection(churches.indexOf(documentSnapshot.get("church")));
-                                });
-                                ArrayAdapter<String> adapter = new ArrayAdapter<>(getContext(), android.R.layout.simple_spinner_item, churches);
-                                adapter.setDropDownViewResource(android.R.layout.simple_dropdown_item_1line);
-                                spinner.setAdapter(adapter);
-                            }
-
-                        } else {
-                            Log.d(TAG, "Error getting documents: ", task.getException());
-                        }
-                    }
-                });
+        fillChurchesSpinner();
 
         spinner.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
             @Override
             public void onItemSelected(AdapterView<?> parent, View view, int i, long id) {
                 church = churches.get(i);
+                parish = parishes.get(i);
             }
 
             @Override
             public void onNothingSelected(AdapterView<?> parent) {
                 church = churches.get(0);
+                parish = parishes.get(0);
             }
         });
 
@@ -113,17 +93,50 @@ public class UserFormFragment extends Fragment {
             user.put("phoneNumber", phoneNumber.getText().toString());
             user.put("email", fAuth.getCurrentUser().getEmail());
             user.put("church", church);
+            user.put("parishId", parish.getId());
+            user.put("parishLoc", parish.getLocation());
 
             userID = fAuth.getCurrentUser().getUid();
             DocumentReference documentReference = fStore.collection("users").document(userID);
 
-            documentReference.set(user).addOnSuccessListener((OnSuccessListener) (aVoid) -> {
+            documentReference.set(user).addOnSuccessListener(aVoid -> {
                 NavHostFragment.findNavController(UserFormFragment.this)
                         .navigate(R.id.action_userFormFragment_to_HomeFragment);
                 Log.d(TAG, "onSuccess: " + userID);
             }).addOnFailureListener(e -> Log.d(TAG, "onFailure: " + e.toString()));
         });
         return view;
+    }
+
+    private void fillChurchesSpinner() {
+        fStore.collection("churches")
+                .get()
+                .addOnCompleteListener(task -> {
+                    if (task.isSuccessful()) {
+                        for (QueryDocumentSnapshot document : task.getResult()) {
+                            churches.add(document.getString("name"));
+                            parishes.add(new Parish(
+                                    document.getId(),
+                                    document.getString("name"),
+                                    document.getGeoPoint("location")
+                            ));
+                        }
+                        if (getActivity() != null) {
+                            usersDocument.addSnapshotListener(getActivity(), (documentSnapshot, e) -> {
+                                phoneNumber.setText(documentSnapshot.getString("phoneNumber"));
+                                name.setText(documentSnapshot.getString("name"));
+                                surname.setText(documentSnapshot.getString("surname"));
+                                spinner.setSelection(churches.indexOf(documentSnapshot.get("church")));
+                            });
+                            ArrayAdapter<String> adapter = new ArrayAdapter<>(getContext(), android.R.layout.simple_spinner_item, churches);
+                            adapter.setDropDownViewResource(android.R.layout.simple_dropdown_item_1line);
+                            spinner.setAdapter(adapter);
+                        }
+
+                    } else {
+                        Log.d(TAG, "Error getting documents: ", task.getException());
+                    }
+                });
     }
 }
 
